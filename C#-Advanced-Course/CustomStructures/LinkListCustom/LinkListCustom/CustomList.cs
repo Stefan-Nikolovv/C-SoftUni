1b7f1afee94723c9f6aae595691783e2c820bab4
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2023-09-06 10:03:18 +0300

    Creating Add method

---
+++ C#-Advanced-Course/CustomStructures/LinkListCustom/LinkListCustom/CustomList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkListCustom
+{
+    public class CustomList
+    {
+        private const int InitialCapacity = 2;
+        private int[] items;
+        public CustomList()
+        {
+            items = new int[InitialCapacity];
+        }
+        public int Count { get; private set; }
+        public int this[int index]
+        { get 
+            { return items[index]; }
+            set { items[index] = value; }
+        }
+        public void Add(int value)
+        {
+            if(items.Length == Count)
+            {
+                Resize();
+            }
+            items[Count] = value;
+            Count++;
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[items.Length * InitialCapacity];
+
+            for(int i = 0; i < Count-1;i++)
+            {
+                copy[i] = items[i];
+            }
+            items = copy;
+        }
+
+        private void InvalidIndex (int index)
+        {
+            if(index < 0 || index >= items.Length)
+            {
+                throw new ArgumentOutOfRangeException($"Invalid {index}");
+            }
+            
+        }
+    }
+}
--- C#-Advanced-Course/CustomStructures/LinkListCustom/LinkListCustom/Program.cs
+++ C#-Advanced-Course/CustomStructures/LinkListCustom/LinkListCustom/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using LinkListCustom;
+
+CustomList customList = new CustomList();
+
+customList.Add(1);
+customList.Add(2);
+customList.Add(3);
+customList.Add(4);
+customList.Add(5);
+customList.Add(6);
+customList.Add(7); 
+customList.Add(8);
