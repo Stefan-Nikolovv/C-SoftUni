aa4249b8e32b1eebd1c6d9ba07b8608d7306a626
Stefan Nikolov <st.nikolov@draftkings.com>
2023-09-25 12:06:02 +0300

    new exam task

---
+++ C#-Advanced-Course/exam Prep 09 Aug/01.MonsterExtermination/01.MonsterExtermination/Program.cs
@@ -0,0 +1,61 @@
+﻿Queue<int> monstersArmor = new(Console.ReadLine()
+    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse));
+
+Stack<int> soldierPower = new(Console.ReadLine()
+    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse));
+
+int killedMonsters = 0;
+int currentValue = 0;
+
+
+while (monstersArmor.Count > 0 && soldierPower.Count > 0)
+{
+    int soldier = soldierPower.Peek();
+    
+    int armor = monstersArmor.Peek();
+    if (soldier >= armor)
+    {
+        killedMonsters++;
+        int currentSoldier = soldierPower.Pop();
+        int currentArmor = monstersArmor.Dequeue();
+        currentValue = currentSoldier - currentArmor;
+        if(!monstersArmor.Any())
+        {
+            break;
+        } 
+        if(currentValue == 0)
+        {
+            continue;
+        }
+        int nextValue = soldierPower.Pop();
+        soldierPower.Push(nextValue + currentValue);
+       
+        
+    }
+    else
+    {
+        int currentSoldier = soldierPower.Pop();
+        int currentArmor = monstersArmor.Dequeue();
+        
+        currentValue = currentArmor - currentSoldier;
+        monstersArmor.Enqueue(currentValue);
+    }
+    
+
+}
+
+if (soldierPower.Any())
+{
+    
+    Console.WriteLine("All monsters have been killed!");
+   
+}
+if(monstersArmor.Any())
+{
+    Console.WriteLine("The soldier has been defeated.");
+   
+}
+
+Console.WriteLine($"Total monsters killed: {killedMonsters}");
---
+++ C#-Advanced-Course/exam Prep 09 Aug/03.VendingSystem/VendingSystem/VendingMachine.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace VendingSystem
+{
+    public class VendingMachine
+    {
+        public int ButtonCapacity;
+        public List<Drink> Drinks;
+        public int GetCount
+        {
+            get { return Drinks.Count; }
+        }
+        
+
+        public VendingMachine(int button)
+        {
+            this.ButtonCapacity = button;
+            Drinks = new List<Drink>();
+        }
+        public void AddDrink(Drink drink)
+        {
+            if(Drinks.Count < ButtonCapacity)
+            {
+                Drinks.Add(drink);
+            }
+        }
+
+        public bool RemoveDrink(string name)
+        {
+          
+           bool isRemoved = Drinks.Remove(Drinks.FirstOrDefault(x => x.Name == name));
+            return isRemoved;
+        }
+        
+
+        public Drink GetLongest()
+        {
+            return Drinks.OrderByDescending(x => x.Volume).FirstOrDefault();
+        }
+
+        public Drink GetCheapest() 
+        {
+            return Drinks.OrderBy(x => x.Price).FirstOrDefault();
+        }
+        public string BuyDrink(string name) 
+        {
+            return Drinks.FirstOrDefault(x => x.Name == name).ToString().TrimEnd();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Drinks available:");
+            foreach (var item in Drinks)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
