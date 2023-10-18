ab0b7b336d2bd830373fdb4a18ad224aee08e76b
Stefan-Nikolovv <106748083+Stefan-Nikolovv@users.noreply.github.com>
2023-10-18 11:35:15 +0300

    yeah

---
+++ C#-Advanced-Course/Regular Exam - 17 June 2023/01.Temple of Doom/01.Temple of Doom/Program.cs
@@ -0,0 +1,57 @@
+﻿Queue<int> tools = new(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+Stack<int> substances = new(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+List<int> challenges = new List<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+
+
+
+while (tools.Any() && substances.Any() && challenges.Any())
+{
+    int currentTool = tools.Dequeue();
+    int currentSub = substances.Pop();
+    int result = currentTool * currentSub;
+
+    if (challenges.Contains(result))
+    {
+     
+        challenges.Remove(result);
+
+    }
+    else 
+    {
+        if(currentSub - 1 > 0)
+        {
+            substances.Push(currentSub - 1);
+        }
+        tools.Enqueue(currentTool + 1);
+    }
+   
+}
+
+if(substances.Count == 0 && challenges.Count > 0)
+{
+    Console.WriteLine("Harry is lost in the temple. Oblivion awaits him.");
+    Console.WriteLine($"Tools: {string.Join(", ", tools)}");
+    Console.WriteLine($"Challenges: {string.Join(", ", challenges)}");
+
+}
+
+
+
+
+if (challenges.Count == 0)
+{
+    Console.WriteLine("Harry found an ostracon, which is dated to the 6th century BCE.");
+    if(substances.Count == 0)
+    {
+        Console.WriteLine($"Tools: {string.Join(", ", tools)}");
+    }
+    if(tools.Count == 0)
+    {
+        Console.WriteLine($"Substances: {string.Join(", ", substances)}");
+    }
+    if(tools.Count > 0 && substances.Count > 0)
+    {
+        Console.WriteLine($"Tools: {string.Join(", ", tools)}");
+        Console.WriteLine($"Substances: {string.Join(", ", substances)}");
+    }
+}
---
+++ C#-Advanced-Course/Regular Exam - 17 June 2023/03.Automotive Repair Shop/AutomotiveRepairShop/RepairShop.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+
+        private int capacity;
+        private List<Vehicle> cars;
+
+        public RepairShop(int capacity)
+        {
+            Cars = new List<Vehicle>();
+            Capacity = capacity;
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if (Cars.Count < this.Capacity)
+            {
+                Cars.Add(vehicle);
+            }
+        }
+        public bool RemoveVehicle(string vin)
+        {
+            Vehicle vehicle = Cars.FirstOrDefault(x => x.VIN == vin);
+            if (vehicle != null)
+            {
+                Cars.Remove(vehicle);
+                return true;
+            }
+            return false;
+        }
+        public int GetCount()
+        {
+            return Cars.Count;
+        }
+
+        public Vehicle GetLowestMileage()
+        {
+            return Cars.MinBy(x => x.MyMileage);
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Vehicles in the preparatory");
+
+            foreach (var car in Cars)
+            {
+                sb.AppendLine(car.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public List<Vehicle> Cars
+        {
+            get { return cars; }
+            set { cars = value; }
+        }
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+    }
+}
