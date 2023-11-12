7eb3ce6397073577db9afea47d22af79f4a8e209
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2023-11-12 16:41:19 +0200

    EF CodeFirst

---
+++ MSSQL/EntityFramework/MappingTables/P02_FootballBetting/P02_FootballBetting.Data.Models/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public static class Constants
+    {
+        public const int ColorNameMaxLength = 100;
+        public const int PositionNameLength = 50;
+        public const int TeamNameLength = 255;
+        public const int TeamInitialsLength = 3;
+    }
+}
---
+++ MSSQL/EntityFramework/Program.cs
@@ -0,0 +1,70 @@
+﻿
+using Entity_Practice.Data;
+namespace SoftUni
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(context));
+            //Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+            //Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                                    .Select(e => new
+                                    {
+                                        e.EmployeeId,
+                                        e.FirstName,
+                                        e.LastName,
+                                        e.MiddleName,
+                                        e.JobTitle,
+                                        e.Salary
+                                    })
+                                      .OrderBy(e => e.EmployeeId)
+                                      .ToList();
+
+            string result = string.Join(Environment.NewLine, employees
+                                                              .Select(e => $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}"));
+            return result;
+        }
+
+        //public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        //{
+        //    var employees = context.Employees.Select(e =>
+        //   new {
+        //        e.FirstName,
+        //        e.Salary
+        //        })
+        //        .Where(s => s.Salary > 50000)
+        //        .OrderBy(n => n.FirstName)
+        //        .ToList();
+
+        //    string result = string.Join(Environment.NewLine, employees.Select(e => $"{e.FirstName} - {e.Salary:f2}"));
+
+
+        //    return result;
+        //}
+
+        //public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        //{
+        //    var employees = context.Employees
+        //                                    .Where(d => d.Department.Name == "Research and Development")
+        //                                     .Select(e => new
+        //                                     {
+        //                                         e.FirstName ,
+        //                                         e.LastName ,
+        //                                        DepartmentName =  e.Department.Name,
+        //                                         e.Salary,
+        //                                     })
+        //                                     .OrderBy(s => s.Salary)
+        //                                     .ThenByDescending(f => f.FirstName)
+        //                                     .ToList();
+        //    return string.Join(Environment.NewLine, employees.Select(e =>
+        //    $"{e.FirstName} {e.LastName} from {e.DepartmentName} - {e.Salary:f2}"));                               
+        //}
+    }
+}
