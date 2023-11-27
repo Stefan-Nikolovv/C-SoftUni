1b9d7faf233eec70e061ee572b292cd3c118e689
Stefan-Nikolovv <106748083+Stefan-Nikolovv@users.noreply.github.com>
2023-11-27 11:31:10 +0200

    Exam Prep 15 August 2022
    Truck

---
+++ MSSQL/Entity Framework/Exam Prep 15 August 2022/Trucks/Data/Models/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trucks.Common;
+
+namespace Trucks.Data.Models
+{
+    public class Client
+    {
+        public Client()
+        {
+            this.ClientsTrucks = new HashSet<ClientTruck>();
+           
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(ValidationConstants.ClientNameMaxLength)]
+        [Required]
+        public string Name { get; set; } = null!;
+        [MaxLength(ValidationConstants.ClientNationalityMaxLength)]
+        [Required]
+        public string Nationality { get; set; } = null!;
+        [Required]
+        public string Type { get; set; } = null!;
+
+        public virtual ICollection<ClientTruck> ClientsTrucks { get; set; } = null!;
+
+    }
+}
---
+++ MSSQL/Entity Framework/Exam Prep 15 August 2022/Trucks/DataProcessor/Deserializer.cs
@@ -0,0 +1,141 @@
+﻿namespace Trucks.DataProcessor
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using System.Xml.Serialization;
+    using Data;
+    using Newtonsoft.Json;
+    using Trucks.Data.Models;
+    using Trucks.Data.Models.Enums;
+    using Trucks.DataProcessor.ImportDto;
+    using Trucks.Utilities;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedDespatcher
+            = "Successfully imported despatcher - {0} with {1} trucks.";
+
+        private const string SuccessfullyImportedClient
+            = "Successfully imported client - {0} with {1} trucks.";
+
+        private static XmlHelper xmlHelper;
+
+        public static string ImportDespatcher(TrucksContext context, string xmlString)
+        {
+            StringBuilder sb = new StringBuilder();
+            xmlHelper = new XmlHelper();
+
+            ImportDespatcherDto[] despatcherDtos =
+                xmlHelper.Deserialize<ImportDespatcherDto[]>(xmlString, "Despatchers");
+
+            ICollection <Despatcher> validDespatcher = new HashSet<Despatcher>();
+            foreach (ImportDespatcherDto despatcherDto in despatcherDtos)
+            {
+                if (!IsValid(despatcherDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                ICollection<Truck> validTrucks = new HashSet<Truck>();
+                foreach (ImportTruckDto truckDto in despatcherDto.Trucks)
+                {
+                    if (!IsValid(truckDto))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Truck truck = new Truck()
+                    {
+                        RegistrationNumber = truckDto.RegistrationNumber,
+                        VinNumber = truckDto.VinNumber,
+                        TankCapacity = truckDto.TankCapacity,
+                        CargoCapacity = truckDto.CargoCapacity,
+                        CategoryType = (CategoryType)truckDto.CategoryType,
+                        MakeType = (MakeType)truckDto.MakeType
+                    };
+                    validTrucks.Add(truck);
+                }
+
+                Despatcher despatcher = new Despatcher()
+                {
+                    Name = despatcherDto.Name,
+                    Position = despatcherDto.Position,
+                    Trucks = validTrucks
+                };
+                validDespatcher.Add(despatcher);
+                sb
+                    .AppendLine(String.Format(SuccessfullyImportedDespatcher, despatcher.Name, validTrucks.Count));
+            }
+
+            context.Despatchers.AddRange(validDespatcher);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+        public static string ImportClient(TrucksContext context, string jsonString)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            ImportClientDto[] clientDtos = JsonConvert.DeserializeObject<ImportClientDto[]>(jsonString);
+
+            ICollection<Client> validClinets = new HashSet<Client>();
+            ICollection<int> truckIdsInDb = context.Trucks
+                .Select(x => x.Id)
+                .ToArray();
+            foreach (ImportClientDto clientDto in clientDtos)
+            {
+                if (!IsValid(clientDto))
+                {
+                    stringBuilder.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                if(clientDto.Type == "usual")
+                {
+                    stringBuilder.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Client client = new Client()
+                {
+                    Name = clientDto.Name,
+                    Nationality = clientDto.Nationality,
+                    Type = clientDto.Type
+
+                };
+
+                foreach (int truckId in clientDto.TruckId.Distinct())
+                {
+                    if (!truckIdsInDb.Contains(truckId))
+                    {
+                        stringBuilder.AppendLine(ErrorMessage);
+                        continue;
+                    }
+                    ClientTruck ct = new ClientTruck()
+                    {
+                        Client = client,
+                        TruckId = truckId,
+                    };
+                    client.ClientsTrucks.Add(ct);
+                }
+                validClinets.Add(client);
+                stringBuilder.AppendLine(String.Format(SuccessfullyImportedClient, client.Name, client.ClientsTrucks.Count));
+            }
+            context.Clients.AddRange(validClinets);
+            context.SaveChanges();
+          return  stringBuilder.ToString().TrimEnd();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
