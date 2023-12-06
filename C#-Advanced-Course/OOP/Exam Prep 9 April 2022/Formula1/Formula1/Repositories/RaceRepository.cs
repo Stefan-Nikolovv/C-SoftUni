73ba3c8c6849e89566caaccb70da5baa22beb0ab
Stefan-Nikolovv <106748083+Stefan-Nikolovv@users.noreply.github.com>
2023-12-06 18:40:36 +0200

    Race Exam 2022

---
+++ C#-Advanced-Course/OOP/Exam Prep 9 April 2022/Formula1/Formula1/Repositories/RaceRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+
+namespace Formula1.Repositories
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        private List<IRace> races;
+        public RaceRepository()
+        {
+            races = new List<IRace>();
+        }
+        public IReadOnlyCollection<IRace> Models => races.AsReadOnly();
+
+        public void Add(IRace model)
+        {
+            races.Add(model);
+        }
+
+        public IRace FindByName(string name)
+        {
+            return races.FirstOrDefault(x => x.RaceName == name);
+        }
+
+        public bool Remove(IRace model)
+        {
+            return races.Remove(model);
+        }
+    }
+}
