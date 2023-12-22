d56ed636e7d71b65953418e46a93738f296baa41
Stefan-Nikolovv <106748083+Stefan-Nikolovv@users.noreply.github.com>
2023-12-22 13:37:14 +0200

    AdvancedCourse

---
+++ C#-Advanced-Course/exam Prep 09 Aug/Exercises Arrays and Lists/MergingLists/Program.cs
@@ -0,0 +1,18 @@
+﻿List <int> firstRow = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+List <int> secondRow = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+List <int> output = new List<int>();
+
+int longerList = Math.Max(firstRow.Count, secondRow.Count);
+
+for (int i = 0; i < longerList; i++)
+{
+    int currentNumber = firstRow[i];
+    output.Add(currentNumber);
+    for (int j = 0; j < secondRow.Count - 1; j++)
+    {
+        int currNumber = secondRow[j];
+        output.Add(currNumber);
+        
+    }
+}
