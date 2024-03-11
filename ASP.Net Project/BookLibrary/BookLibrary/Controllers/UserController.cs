0e363d7174d55b1c8d785c18eafe9b9e7d7be6c3
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2024-03-11 18:50:13 +0200

    Added UserController

---
+++ ASP.Net Project/BookLibrary/BookLibrary/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookLibrary.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+    }
+}
