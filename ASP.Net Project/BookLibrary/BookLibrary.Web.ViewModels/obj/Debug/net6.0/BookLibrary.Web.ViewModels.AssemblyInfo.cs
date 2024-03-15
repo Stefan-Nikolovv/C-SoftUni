a1ee1b720d1c49713154f366299d19bd17f5d186
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2024-03-15 19:26:15 +0200

    added

--- ASP.Net Project/BookLibrary/BookLibrary.Web.ViewModels/obj/Debug/net6.0/BookLibrary.Web.ViewModels.AssemblyInfo.cs
+++ ASP.Net Project/BookLibrary/BookLibrary.Web.ViewModels/obj/Debug/net6.0/BookLibrary.Web.ViewModels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookLibrary.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4221206ec6c8a64299100d5cbdc9834c5a8e9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookLibrary.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookLibrary.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
